8716342073c41466a3149f1fce7fe6649c795c6d
santhoshkumarravichandran17-glitch <santhoshkumarravichandran17@gmail.com>
2026-03-17 11:16:51 +0530

    assignment

---
+++ sample1.py
@@ -0,0 +1,9 @@
+# Function to calculate total price
+
+def total_price(price, quantity):
+    total = price * quantity
+    return total
+
+result = total_price(100, 3)
+
+print("Total price:", result)
