a606754174d335a36cb384f2a657113f3d0b41a4
santhoshkumarravichandran17-glitch <santhoshkumarravichandran17@gmail.com>
2026-03-18 08:34:44 +0530

    assignment

---
+++ Day 16.py
@@ -0,0 +1,41 @@
+import threading
+import time
+
+def write_data():
+    fp = open("students.txt", "w")
+    fp.write("1 Dinesh\n")
+    time.sleep(1)
+    fp.write("2 Kumar\n")
+    time.sleep(1)
+    fp.close()
+    print("Writing done")
+
+def read_data():
+    time.sleep(2)  # wait for writing to start
+    fp = open("students.txt", "r")
+    print(fp.read())
+    fp.close()
+    print("Reading done")
+
+t1 = threading.Thread(target=write_data)
+t2 = threading.Thread(target=read_data)
+
+t1.start()
+t2.start()
+
+
+fp = open("students.txt", "w")
+
+fp.write("1 Dinesh\n")
+fp.write("2 Kumar\n")
+
+fp.close()
+
+print("Written successfully")
+
+fp = open("students.txt", "r")
+
+data = fp.read()
+print(data)
+
+fp.close()
